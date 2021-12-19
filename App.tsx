65fb631c06f7de07a4ac3c4509625ec61f0c8302
ipins46 <muhamadarifin428@gmail.com>
2021-12-20 01:23:40 +0700

    auth

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding from './src/Authentication/Onboarding';
+
+const AuthenticationStack = createStackNavigator();
+const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator>
+      <AuthenticationStack.Screen name='OnBoarding' component={Onboarding}/>
+    </AuthenticationStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <AuthenticationNavigator />
+    </NavigationContainer>
   );
 }
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+interface ComponentNameProps {}
+
+const ComponenName = () => {
+  return <View style={flex:1, backgroundColor: "cyan"} />;
+};
+
+export default ComponenName;
